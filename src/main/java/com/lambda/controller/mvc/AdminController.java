46ca4ca96d860b3571a1831969b66dc05b2c18d0
thanhvt <thanhvt@vissoft.vn>
2021-09-06 02:51:30 +0700

    add ping controller

--- src/main/java/com/lambda/controller/mvc/AdminController.java
+++ src/main/java/com/lambda/controller/mvc/AdminController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class AdminController {
 
     @PostMapping("/login")
